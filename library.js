275bd3c8630f9b043fe2ea59ca19782ca90f8f28
Miwi35 <meven35@gmail.com>
2014-03-19 19:22:46 +0100

    Create library.js

---
+++ library.js
@@ -0,0 +1,19 @@
+//library.js
+
+var Imgbed = {},
+    XRegExp = require('xregexp').XRegExp,
+    extensions = ['jpg', 'jpeg', 'gif', 'png'],  // add capability for control panel here
+    regexStr = '<a href="(https?://[^\.]+\.[^\/]+\/[^\.]+\.(' + extensions.join('|') + '))">[^<]*<\/a>';
+
+// declare regex as global and case-insensitive
+var regex = XRegExp(regexStr, 'gi');
+
+Imgbed.parse = function(postContent, callback){
+    if (postContent.match(regex)){
+        postContent = postContent.replace(regex, '<img src="$1" alt="$1">');
+    }
+    callback(null, postContent);
+}
+
+
+module.exports = Imgbed;
